e71a23f9cff942c620083e2c50416736bf5567f0
Deborah S Navarro <deborah.scavacini@gmail.com>
2025-08-30 20:39:35 -0300

    acrescentando readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # conversorTemperatura
+Nome dos proprietários do projeto: Deborah Scavacini Navarro e Juliana Aguilar
+
+Descrição do trabalho: Desenvolver uma aplicação que converte temperatura entre Celsius, Kelvin e Fahrenheit. Projeto realizado em HTML/CSS e JavaScript.
---
+++ js/conversor.js
@@ -0,0 +1,14 @@
+/**
+ * Converte a temperatura entre Celsius, Kelvin e Fahrenheit
+ * @author Deborah S Navarro e Juliana Aguilar <deborah.scavacini@gmail.com>
+ * @version 1.0.0
+ * @since 1.0.0 (30/08/2025)
+ * @param {number} celsius - graus celsius
+ * @param {number} kelvin - graus kelvin
+ * @param {number} fahrenheit - graus fahrenheit
+ * @returns {number} o valor do convertido calculado
+ * 
+ * @example //converte 36ºC em Fahrenheit
+ * const resultado = calcularTemperatura(36,0)
+ * console.log(resultado)
+ */
